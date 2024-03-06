d678b7a253e07d992d652bee1035b5d40860ab9f
nschcur <nazae530@gmail.com>
2024-03-06 21:56:24 +0200

    creating models Actor and Genre

--- theatre/models.py
+++ theatre/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 
 class Play(models.Model):
-    id = models.IntegerField()
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField('Actor', related_name='plays')
+    genres = models.ManyToManyField('Genre', related_name='plays')
 
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    lsat_name = models.CharField(max_length=255)
+
+
+class Genre(models.Model):
+    name = models.CharField()
